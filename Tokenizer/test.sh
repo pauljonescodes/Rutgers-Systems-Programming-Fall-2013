c8b64253f410f400e3e87749312c6e8b974c5e44
Paul Jones <paulmmj@gmail.com>
2013-09-18 12:20:01 -0400

    added automated testing

---
+++ Tokenizer/test.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+make
+
+actual=$(./tokenizer "\n" "hello\nworld")
+
+expect="hello
+world"
+
+if [ "$actual" == "$expect" ]; then
+  echo "passed 1"
+else
+  echo "failed 1"
+fi
+
+actual=$(./tokenizer "m" "mmmmm")
+
+expect=""
+
+if [ "$actual" == "$expect" ]; then
+  echo "passed 2"
+else
+  echo "failed 2"
+fi
+
+actual=$(./tokenizer "e\v\t " "hel\vlo \tworld")
+
+expect="h
+l
+lo
+world"
+
+if [ "$actual" == "$expect" ]; then
+  echo "passed 3"
+else
+  echo "failed 3"
+fi
+
+actual=$(./tokenizer "" "You have to print out hex of \a")
+
+expect="You have to print out hex of [0x07]"
+
+if [ "$actual" == "$expect" ]; then
+  echo "passed 4"
+else
+  echo "failed 4"
+fi
+
+actual=$(./tokenizer "\n" "")
+expect=""
+
+if [ "$actual" == "$expect" ]; then
+  echo "passed 5"
+else
+  echo "failed 5"
+fi
+
+actual=$(./tokenizer "\k" "hello\kworld")
+
+expect="hello
+world"
+
+if [ "$actual" == "$expect" ]; then
+  echo "passed 6"
+else
+  echo "failed 6"
+fi
+
+actual=$(./tokenizer "k" "hello\kworld")
+
+expect="hello
+world"
+
+if [ "$actual" == "$expect" ]; then
+  echo "passed 7"
+else
+  echo "failed 7"
+fi
+
+actual=$(./tokenizer "\\\\" "\\\\\\hello\\\\\\world\\\\")
+
+expect="hello
+world"
+
+if [ "$actual" == "$expect" ]; then
+  echo "passed 8"
+else
+  echo "failed 8"
+fi
+
+actual=$(./tokenizer "\b\le" "ShE sells sEa shells by the sEa shorE\\")
+
+expect="ShE ss sEa shsy the sEa shorE"
+
+if [ "$actual" == "$expect" ]; then
+  echo "passed 9"
+else
+  echo "failed 9"
+fi
+
+actual=$(./tokenizer "\k\\" "hello\kworld")
+
+expect="hello
+world"
+
+if [ "$actual" == "$expect" ]; then
+  echo "passed 10"
+else
+  echo "failed 10"
+fi
+
+# actual=$(./tokenizer "" "\\\"")
+
+make clean
