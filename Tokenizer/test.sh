7e953d1afce536724ad27ce45318f7e64bb54e65
Paul Jones <paulmmj@gmail.com>
2013-09-20 22:23:48 -0400

    final assignment 2 commit

--- Tokenizer/test.sh
+++ Tokenizer/test.sh
@@ -44,10 +44,6 @@ if [ "$actual" == "$expect" ]; then
   echo "passed 4"
 else
   echo "failed 4"
-  echo "actual: 
-$actual"
-  echo "expect: 
-$expect"
 fi
 
 actual=$(./tokenizer "\n" "")
@@ -114,6 +110,59 @@ if [ "$actual" == "$expect" ]; then
   echo "passed 10"
 else
   echo "failed 10"
+fi
+
+actual=$(./tokenizer "" "\a \b \n \t")
+
+expect="[0x07] [0x08] [0x0A] [0x09]"
+
+if [ "$actual" == "$expect" ]; then
+  echo "passed 11"
+else
+  echo "failed 11"
+fi
+
+actual=$(./tokenizer "" "ShE sells sEa shells by the sEa shorE")
+
+expect="ShE sells sEa shells by the sEa shorE"
+
+if [ "$actual" == "$expect" ]; then
+  echo "passed 12"
+else
+  echo "failed 12"
+fi
+
+actual=$(./tokenizer "\v\t" "hel\vlo \tworld")
+
+expect="hel
+lo 
+world"
+
+if [ "$actual" == "$expect" ]; then
+  echo "passed 13"
+else
+  echo "failed 13"
+fi
+
+actual=$(./tokenizer "\b" "hello\bworld")
+
+expect="hello
+world"
+
+if [ "$actual" == "$expect" ]; then
+  echo "passed 14"
+else
+  echo "failed 14"
+fi
+
+actual=$(./tokenizer "\b" "\b\b\b\b\b")
+
+expect=""
+
+if [ "$actual" == "$expect" ]; then
+  echo "passed 15"
+else
+  echo "failed 15"
 fi
 
 # actual=$(./tokenizer "" "\\\"")
