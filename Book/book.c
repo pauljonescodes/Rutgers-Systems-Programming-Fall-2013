ce0123d34350c80d47fd66f142dad13f41c4be41
yurikahn <codecapp13@gmail.com>
2013-11-09 20:49:09 -0500

    first commit - parser

---
+++ Book/book.c
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include "tokenizer.h"
+
+#define error( fprintf(stderr,
+#define debug printf("LINE = %d\n",__LINE__);
+#define FAIL 1
+#define SUCCESS 0
+
+/* A pseudo-type to hold catagory values. */
+typedef char* catagory_t;
+
+/* Struct with information about a consumer */
+typedef struct consumer_t_ {
+	int 		id;		/* The numerical ID of this customer. */
+	char* 		name;		/* The customer's name */
+	char* 		address;	/* The customer's street address */
+	char* 		state;		/* The customer's state */
+	char* 		zip;		/* The customer's zip code */
+	double 		credit;		/* The customer's current account balence ($) */
+}consumer_t;
+
+/* Struct with informatio about a book order */
+typedef struct order_t_
+	double		cost;		/* The cost of the book. */
+	char*		title;		/* The book's title */
+	int 		customerId;	/* The ID of the ordering customer */
+	catagory_t 	catagory;	/* The catagory listed for this book */
+}order_t;
+
+/* Function prototypes */
+int 		processCatagories(catagory_t*, char*);			/* processes the catagory input list into an array */
+int 		processDatabase(FILE*, consumer_t*);			/* parses the database file into a list of consumers */
+int 		processOrders(FILE*, order_t*, catagory_t*);		/* parses the order file into a list of orders to be executed */
+int 		getFileLength(FILE*);					/* returns the length of the passed file */
+void* 		cleanMalloc(size_t);					/* returns allocated memory of all 0's */
+char*		trim(char*);						/* trims first and last characters */
+char* 		clean(char*); 						/* trims leading and tailing whitespace */
+
+char* trim(char* s) {
+	s++;
+	s[strlen(s) - 1] = 0;
+	return s;
+}
+
+char* clean(char* s) {
+ 	while(s[0] == ' ') {
+		s++;
+	}
+	while(s[strlen(s) - 1] == ' ') {
+		s[strlen(s) - 1] = 0;
+	}
+	return s;
+}
+
+void* cleanMalloc(size_t size) {
+	void* 		chunk;		/* requested chunk of memory */
+	
+	chunk = malloc(size);
+	memset(chunk,0,size);
+	return chunk;
+}
+
+int getFileLength(FILE* f) {
+	int 		POS;		/* The saved current position */
+	int 		LEN;		/* The length of the file */
+	
+	POS = ftell(f);
+	fseek(f, 0, SEEK_END);
+	LEN = ftell(f);
+	fseek(f, POS, SEEK_SET);
+	return LEN;
+}
+
+int processCatagories(catagory_t* catList, char* s) {
+	TokenizerT* 	tk;		/* Tokenizer used to split the input string */
+	
+	tk = TKCreate(s, 0, NULL);
+	if(tk == 0) { return FAIL; }
+	catList = (catagory_t*) tk->tokens;
+	return SUCCESS;
+}
+
+int processDatabase(FILE* DATABASE, consumer_t* consumerData) {
+	char* 		file_string;	/* string containing all the contents of the database */
+	int 		database_size;	/* size (number of characters) of the database */
+	TokenizerT* 	tk;		/* tokenizer for the database */
+	char** 		lines;		/* list of lines in the database */
+	char** 		items;		/* reused array for each line; the properties of each consumer */
+	int 		numlines;	/* number of lines */
+	int 		i;		/* iterator */
+	
+	database_size = getFileLength(DATABASE);
+	file_string = cleanMalloc(database_size + 1);
+	fread(file_string, 1, database_size, DATABASE);
+	fclose(DATABASE);
+	
+	tk = TKCreate(file_string, 1, "\n");
+	lines = tk->tokens;
+	numlines = tk->num_tok;
+	consumerData = cleanMalloc(numlines * sizeof(consumer_t));
+	
+	for(i=0;i<numlines;i++) {
+		tk = TKCreate(lines[i], 1, "|");
+		items = tk->tokens;
+		if(tk->num_tok != 6) { return FAIL; }
+		consumerData[i].name 	= trim(clean(items[0]));
+		consumerData[i].id   	= atoi(clean(items[1]));
+		consumerData[i].credit 	= atof(clean(items[2]));
+		consumerData[i].address = trim(clean(items[3]));
+		consumerData[i].state 	= trim(clean(items[4]));
+		consumerData[i].zip	= trim(clean(items[5]));
+	}
+
+	return SUCCESS;
+}
+
+int processOrders(FILE* ORDER, order_t* orderData, catagory_t* catList) {
+	char* 		file_string;	/* string containing all the contents of the database */
+	int 		database_size;	/* size (number of characters) of the database */
+	TokenizerT* 	tk;		/* tokenizer for the database */
+	char** 		lines;		/* list of lines in the database */
+	char** 		items;		/* reused array for each line; the properties of each consumer */
+	int 		numlines;	/* number of lines */
+	int 		i;		/* iterator */
+	
+	database_size = getFileLength(DATABASE);
+	file_string = cleanMalloc(database_size + 1);
+	fread(file_string, 1, database_size, DATABASE);
+	fclose(DATABASE);
+	
+	tk = TKCreate(file_string, 1, "\n");
+	lines = tk->tokens;
+	numlines = tk->num_tok;
+	consumerData = cleanMalloc(numlines * sizeof(consumer_t));
+	
+	for(i=0;i<numlines;i++) {
+		tk = TKCreate(lines[i], 1, "|");
+		items = tk->tokens;
+		if(tk->num_tok != 6) { return FAIL; }
+		orderData[i].name 	= trim(clean(items[0]));
+		orderData[i].id   	= atoi(clean(items[1]));
+		orderData[i].credit 	= atof(clean(items[2]));
+		orderData[i].address 	= trim(clean(items[3]));
+		orderData[i].state 	= trim(clean(items[4]));
+		orderData[i].zip	= trim(clean(items[5]));
+	}
+
+	return SUCCESS;
+
+}
+
+int main(int argc, char** argv) {
+	/* Input handing / error checking. */
+	FILE* 		DATABASE;	/* File pointer to the customer database */
+	FILE* 		ORDER;		/* File pointer to the order database */
+	consumer_t* 	consumerData;	/* Data held about consumers, parsed from the database file */
+	order_t*	orderData; 	/* List of all book orders, parsed from the order list file */
+	catagory_t* 	catList;	/* List of catagories. */
+	
+	if(argc != 4) {
+		error("ERROR: Invalid number of arguments.\n\tPlease see readme for usage information.\n");
+		return FAIL;
+	}
+	
+	DATABASE = fopen(argv[1], "r+");
+	if(DATABASE == 0) {
+		error("ERROR: Database input file \'%s\' does not exist.\n", argv[1]);
+		return FAIL;
+	}
+	ORDER = fopen(argv[2], "r+");
+	if(ORDER == 0) {
+		error("ERROR: Book order file \'%s\' does not exist.\n", argv[2]);
+		return FAIL;
+	}
+	
+	if(processCatagories(catList)) 			{ return FAIL; }
+	if(processDatabase(DATABASE, consumerData)) 	{ return FAIL; }
+	if(processOrders(ORDER, orderData, catList)) 	{ return FAIL; }
+	
+	return SUCCESS;
+}
---
+++ Book/tokenizer.h
@@ -0,0 +1,27 @@
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+#include <stdlib.h>
+
+/*
+ * Tokenizer type.  You need to fill in the type as part of your implementation.
+ */
+
+/*typedef struct TokenizerT_ {
+	char* copied_string;
+	char* delimiters;
+	char* current_position;
+} TokenizerT;*/
+
+/* Tokenizer struct */
+typedef struct TokenizerT_ {
+	int token_index; /* This keeps track of the token that this object is on for the nextToken feature */
+	char** tokens;   /* filled in by TKCreate; a list of the tokens to be iterated through */
+	int num_tok;     /* number of tokens */
+}TokenizerT;
+
+TokenizerT *TKCreate(char *ts);
+void TKDestroy(TokenizerT *tk);
+char *TKGetNextToken(TokenizerT *tk);
+
+#endif
