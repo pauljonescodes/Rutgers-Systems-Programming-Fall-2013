0e6d86a6d95d1ffaa6e9e4ab250681051a9a5710
Paul Jones <paulmmj@gmail.com>
2013-12-18 14:43:34 -0500

    cheat sheet initial

---
+++ Cheat Sheet/snippets/funcpoint.c
@@ -0,0 +1,10 @@
+void func(int);
+main() {
+    void (*fp)(int);
+    fp = func;
+    (*fp)(1);
+    fp(2);
+    exit(EXIT_SUCCESS); }
+void func(int arg){
+    printf("%d\n", arg);
+}
---
+++ Cheat Sheet/snippets/signal.c
@@ -0,0 +1,14 @@
+FILE *temp_file;
+
+void leave(int sig) {
+	fprintf(temp_file,"\nInterrupted..\n");
+	fclose(temp_file);
+	exit(sig); }
+main() {
+	(void) signal(SIGINT,leave);
+	temp_file = fopen("tmp","w");
+	for(;;) {
+	        printf("Ready...\n");
+	        (void)getchar();
+	}
+	exit(EXIT_SUCCESS); }
